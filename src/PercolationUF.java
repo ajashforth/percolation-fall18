d673d52e89ff9ecaf58457422061d5dd4b311aea
ajashforth <alecashforth@gmail.com>
2018-12-05 22:23:11 -0500

    UF5

--- src/PercolationUF.java
+++ src/PercolationUF.java
@@ -30,16 +30,16 @@ public class PercolationUF implements IPercolate{
 			else if(col == myGrid.length - 1) {
 				myFinder.union(row * myGrid.length + col, VBOTTOM);
 			}
-			if(col >= 0 && isOpen(row, col - 1)) {
+			if(col - 1 >= 0 && isOpen(row, col - 1)) {
 				myFinder.union(row * myGrid.length + col, row * myGrid.length + col - 1);
 			}
-			if(col < myGrid.length && isOpen(row, col + 1)) {
+			if(col + 1 < myGrid.length && isOpen(row, col + 1)) {
 				myFinder.union(row * myGrid.length + col, row * myGrid.length + col + 1);
 			}
-			if(row >= 0 && isOpen(row - 1, col)) {
+			if(row - 1 >= 0 && isOpen(row - 1, col)) {
 				myFinder.union(row * myGrid.length + col, (row-1) * myGrid.length + col);
 			}
-			if(row < myGrid.length && isOpen(row + 1, col)) {
+			if(row + 1 < myGrid.length && isOpen(row + 1, col)) {
 				myFinder.union(row * myGrid.length + col, (row+1) * myGrid.length + col - 1);
 			}
 		}
